4415693ff46404f0afc4ecf3e081b54aedf6ec49
VoidScientist <louthosi59@gmail.com>
2023-04-25 23:29:54 +0200

    fixed an oopsie

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 function changePage(arg) {
     console.log(window.location.host)
-    if (window.location.pathname != document.location.origin + arg) {
-        window.location.pathname = document.location.origin + arg;
+    if (window.location.pathname != document.location.origin + "/" + arg) {
+        window.location.pathname = document.location.origin + "/" + arg;
     }
 }
