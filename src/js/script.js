6d8e068d1562c9cd95bdae9f32cf497493d6bcfb
VoidScientist <louthosi59@gmail.com>
2023-06-23 11:24:46 +0200

    this will quickly get reverted

--- src/js/script.js
+++
@@ -1,6 +0,0 @@
-function changePage(arg) {
-    console.log(window.location.host)
-    if (window.location.pathname != document.location.origin + "/" + arg) {
-        window.location.pathname = document.location.origin + "/" + arg;
-    }
-}
